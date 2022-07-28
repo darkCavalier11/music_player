66c5174b5f8c48a8cea76e9c12d78491ef3f04ce
darkCavalier11 <57953014+darkCavalier11@users.noreply.github.com>
2022-07-28 22:42:08 +0530

    added audio player

--- lib/redux/models/app_state.dart
+++ lib/redux/models/app_state.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:just_audio/just_audio.dart';
 
 class AppState {
   final UiState uiState;
@@ -36,32 +37,39 @@ class AppState {
 
 class UiState {
   final ThemeMode themeMode;
+  final AudioPlayer audioPlayer;
   UiState({
+    required this.audioPlayer,
     required this.themeMode,
   });
 
   factory UiState.initial() {
-    return UiState(themeMode: ThemeMode.light);
+    return UiState(themeMode: ThemeMode.light, audioPlayer: AudioPlayer());
   }
 
   UiState copyWith({
     ThemeMode? themeMode,
+    AudioPlayer? audioPlayer,
   }) {
     return UiState(
       themeMode: themeMode ?? this.themeMode,
+      audioPlayer: audioPlayer ?? this.audioPlayer,
     );
   }
 
   @override
-  String toString() => 'UiState(themeMode: $themeMode)';
+  String toString() =>
+      'UiState(themeMode: $themeMode, audioPlayer: $audioPlayer)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is UiState && other.themeMode == themeMode;
+    return other is UiState &&
+        other.themeMode == themeMode &&
+        other.audioPlayer == audioPlayer;
   }
 
   @override
-  int get hashCode => themeMode.hashCode;
+  int get hashCode => themeMode.hashCode ^ audioPlayer.hashCode;
 }
