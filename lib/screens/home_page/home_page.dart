b313a5e56f206276180083efe7eed23ea80e3c2d
darkCavalier11 <57953014+darkCavalier11@users.noreply.github.com>
2022-08-01 23:59:13 +0530

    added loading indicator

--- lib/screens/home_page/home_page.dart
+++ lib/screens/home_page/home_page.dart
@@ -1,4 +1,5 @@
 import 'dart:developer';
+import 'dart:math';
 
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/cupertino.dart';
@@ -80,6 +81,7 @@ class HomePage extends StatelessWidget {
                     ),
                   ),
                   const Divider(),
+                  Loading(),
                 ],
               ),
               BottomNavigationCluster()
@@ -106,6 +108,81 @@ class HomePage extends StatelessWidget {
           //   },
           //   child: Icon(Icons.play_arrow_rounded),
           // ),
+        );
+      },
+    );
+  }
+}
+
+class Loading extends StatefulWidget {
+  const Loading({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<Loading> createState() => _AnimatedMusicWaveState();
+}
+
+class _AnimatedMusicWaveState extends State<Loading>
+    with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _sizeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      reverseDuration: const Duration(seconds: 1),
+      duration: const Duration(seconds: 1),
+    )..repeat(reverse: true);
+
+    _sizeAnimation =
+        Tween<double>(begin: 0, end: 10).animate(_animationController);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _sizeAnimation,
+      builder: (context, child) {
+        return Row(
+          children: [
+            const SizedBox(width: 20),
+            Container(
+              margin: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3),
+                color: Theme.of(context).primaryColor,
+              ),
+              height: 16 * sin(3 + _sizeAnimation.value + 40) + 20,
+              width: 16,
+            ),
+            Container(
+              margin: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3),
+                color: Theme.of(context).primaryColor,
+              ),
+              height: 16 * cos(3 + _sizeAnimation.value + 10) + 30,
+              width: 16,
+            ),
+            Container(
+              margin: const EdgeInsets.all(1),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(3),
+                color: Theme.of(context).primaryColor,
+              ),
+              height: 3 + 4 * _sizeAnimation.value,
+              width: 16,
+            ),
+          ],
         );
       },
     );
