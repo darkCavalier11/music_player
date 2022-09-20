8aee313e21b60219ff943814caff945cb6256621
darCavalier11 <sumit.pradhan65@gmail.com>
2022-09-21 00:06:40 +0530

    added music disc

--- lib/widgets/bottom_navigation.dart
+++ lib/widgets/bottom_navigation.dart
@@ -1,7 +1,9 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:developer';
+import 'dart:math' hide log;
 
 import 'package:async_redux/async_redux.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
@@ -9,6 +11,8 @@ import 'package:just_audio/just_audio.dart';
 
 import 'package:music_player/redux/action/ui_action.dart';
 import 'package:music_player/redux/models/app_state.dart';
+
+import '../redux/models/music_item.dart';
 
 class BottomNavigationWidget extends StatelessWidget {
   const BottomNavigationWidget({Key? key}) : super(key: key);
@@ -27,7 +31,7 @@ class BottomNavigationWidget extends StatelessWidget {
               return Row(
                 children: [
                   AnimatedContainer(
-                    duration: const Duration(seconds: 1),
+                    duration: const Duration(milliseconds: 200),
                     width: MediaQuery.of(context).size.width *
                         (musicSnapshot.data! == ProcessingState.idle
                             ? 0.6
@@ -81,18 +85,8 @@ class BottomNavigationWidget extends StatelessWidget {
                               width: 1,
                               color: Theme.of(context).hintColor,
                             ),
-                            GestureDetector(
-                              onTap: () {
-                                // snapshot.onChange(2);
-                              },
-                              child: Container(
-                                width: 30,
-                                height: 30,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  color: Colors.pinkAccent,
-                                ),
-                              ),
+                            MusicPlayingSmallIndicator(
+                              imageUrl: snapshot.musicItem!.imageUrl,
                             ),
                           ]
                         ],
@@ -102,6 +96,87 @@ class BottomNavigationWidget extends StatelessWidget {
                 ],
               );
             });
+      },
+    );
+  }
+}
+
+class MusicPlayingSmallIndicator extends StatefulWidget {
+  final String imageUrl;
+  const MusicPlayingSmallIndicator({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  @override
+  State<MusicPlayingSmallIndicator> createState() =>
+      _MusicPlayingSmallIndicatorState();
+}
+
+class _MusicPlayingSmallIndicatorState extends State<MusicPlayingSmallIndicator>
+    with SingleTickerProviderStateMixin {
+  @override
+  late AnimationController _animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 8),
+    );
+    _animationController.forward();
+    _animationController.repeat(reverse: false);
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedBuilder(
+      animation: _animationController,
+      child: GestureDetector(
+        onTap: () {
+          // todo : expand to full playing screen
+          // snapshot.onChange(2);
+        },
+        child: Container(
+          width: 30,
+          height: 30,
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            image: DecorationImage(
+              image: CachedNetworkImageProvider(
+                widget.imageUrl,
+              ),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Center(
+            child: Container(
+              width: 10,
+              height: 10,
+              decoration: BoxDecoration(
+                color: Colors.black,
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.white,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      builder: (context, child) {
+        return Transform.rotate(
+          angle: _animationController.value * 2 * pi,
+          child: child,
+        );
       },
     );
   }
@@ -171,10 +246,12 @@ class _ViewModel extends Vm {
   final int currentBottomNavIndex;
   final void Function(int) onChange;
   final Stream<ProcessingState> musicPlayingState;
+  final MusicItem? musicItem;
   _ViewModel({
     required this.currentBottomNavIndex,
     required this.onChange,
     required this.musicPlayingState,
+    required this.musicItem,
   });
 }
 
@@ -183,6 +260,7 @@ class _Factory extends VmFactory<AppState, BottomNavigationWidget> {
   @override
   _ViewModel fromStore() {
     return _ViewModel(
+      musicItem: state.audioPlayerState.selectedMusic,
       currentBottomNavIndex: state.uiState.currentBottomNavIndex,
       onChange: (index) {
         dispatch(ChangeBottomNavIndex(index: index));
