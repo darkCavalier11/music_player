4755a263ddc72c7e9f6fc32de08712ddf3302a56
darCavalier11 <sumit.pradhan65@gmail.com>
2022-09-20 23:27:39 +0530

    added music playlist tile

--- lib/screens/playlist_screen/widgets/playlist_item_tile.dart
+++ lib/screens/playlist_screen/widgets/playlist_item_tile.dart
@@ -1,9 +1,18 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class PlaylistItemTile extends StatelessWidget {
+  final String imageUrl1;
+  final String? imageUrl2;
+  final String? imageUrl3;
+
   const PlaylistItemTile({
     Key? key,
+    required this.imageUrl1,
+    this.imageUrl2,
+    this.imageUrl3,
   }) : super(key: key);
 
   @override
@@ -16,49 +25,51 @@ class PlaylistItemTile extends StatelessWidget {
             width: 80,
             child: Stack(
               children: [
-                Positioned(
-                  left: 20,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: ColorFiltered(
-                      colorFilter: ColorFilter.mode(
-                        Theme.of(context).disabledColor,
-                        BlendMode.modulate,
-                      ),
-                      child: Image.network(
-                        'https://images.unsplash.com/photo-1513010963904-2fefe6a92780?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                        height: 60,
-                        width: 60,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 10,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: ColorFiltered(
-                      colorFilter: ColorFilter.mode(
-                        Theme.of(context).hintColor,
-                        BlendMode.modulate,
-                      ),
-                      child: Image.network(
-                        'https://images.unsplash.com/photo-1663431261867-01399cf8847d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-                        height: 60,
-                        width: 60,
-                        fit: BoxFit.cover,
+                if (imageUrl3 != null)
+                  Positioned(
+                    left: 20,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          Theme.of(context).disabledColor,
+                          BlendMode.modulate,
+                        ),
+                        child: Image.network(
+                          imageUrl3!,
+                          height: 60,
+                          width: 60,
+                          fit: BoxFit.cover,
+                        ),
                       ),
                     ),
                   ),
-                ),
+                if (imageUrl2 != null)
+                  Positioned(
+                    left: 10,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(100),
+                      child: ColorFiltered(
+                        colorFilter: ColorFilter.mode(
+                          Theme.of(context).hintColor,
+                          BlendMode.modulate,
+                        ),
+                        child: Image(
+                          image: CachedNetworkImageProvider(imageUrl2 ?? ''),
+                          height: 60,
+                          width: 60,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                  ),
                 Stack(
                   alignment: Alignment.center,
                   children: [
                     ClipRRect(
                       borderRadius: BorderRadius.circular(100),
-                      child: Image.network(
-                        'https://images.unsplash.com/photo-1663475928373-12f26f48e4b4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+                      child: Image(
+                        image: CachedNetworkImageProvider(imageUrl1),
                         height: 60,
                         width: 60,
                         fit: BoxFit.cover,
@@ -68,12 +79,13 @@ class PlaylistItemTile extends StatelessWidget {
                       width: 30,
                       height: 30,
                       decoration: BoxDecoration(
-                          color: Theme.of(context).scaffoldBackgroundColor,
-                          shape: BoxShape.circle,
-                          border: Border.all(
-                            color: Colors.white,
-                            width: 2,
-                          )),
+                        color: Theme.of(context).scaffoldBackgroundColor,
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.white,
+                          width: 2,
+                        ),
+                      ),
                     ),
                     const Icon(
                       CupertinoIcons.play_arrow_solid,
