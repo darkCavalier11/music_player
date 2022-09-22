8c11bc89bd98b8dcefd3e314c5f352ee372c6c23
darCavalier11 <sumit.pradhan65@gmail.com>
2022-09-22 21:40:44 +0530

    added get music home item

--- lib/screens/home_screen/actions/home_screen_actions.dart
+++ lib/screens/home_screen/actions/home_screen_actions.dart
@@ -1,48 +1,20 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
-import 'dart:convert';
 import 'dart:developer';
 
 import 'package:async_redux/async_redux.dart';
 
 import 'package:music_player/redux/models/app_state.dart';
-import 'package:music_player/redux/models/music_filter_payload.dart';
 import 'package:music_player/redux/models/search_state.dart';
-import 'package:music_player/utils/api_request.dart';
-import 'package:music_player/utils/app_db.dart';
-import 'package:music_player/utils/url.dart';
+import 'package:music_player/utils/yt_parser/lib/parser_helper.dart';
 
-import '../../../redux/models/music_item.dart';
 
 class LoadHomePageMusicAction extends ReduxAction<AppState> {
   @override
   Future<AppState?> reduce() async {
     try {
       _SetHomeScreenLoadingAction(loadingState: LoadingState.loading);
-      final tempRes = await ApiRequest.get(AppUrl.loadPayloadForFilterUrl);
-      final musicFilterPayload =
-          MusicFilterPayloadModel.fromApiResponse(tempRes);
-      await AppDatabse.setQuery(
-          DbKeys.context, jsonEncode(musicFilterPayload.toJson()));
-      final res =
-          await ApiRequest.post(AppUrl.browseUrl(musicFilterPayload.apiKey), {
-        'context': musicFilterPayload.context.toJson(),
-        'continuation': musicFilterPayload.continuation
-      });
-      log(res.toString());
-      final json = jsonDecode(res.data.toString());
-      final items = json['onResponseReceivedActions'][0]
-          ['reloadContinuationItemsCommand']['continuationItems'] as List;
-      final List<MusicItem> homeScreenMusicItems = [];
-      for (var item in items) {
-        final musicItem = item['richItemRenderer'];
-        if (musicItem != null && musicItem['content'] != null) {
-          if (musicItem['content']['videoRenderer'] != null) {
-            homeScreenMusicItems.add(
-                MusicItem.fromApiJson(musicItem['content']['videoRenderer']));
-          } else {}
-        }
-      }
+      final homeScreenMusicItems = await ParserHelper.getHomeScreenMusic();
       _SetHomeScreenLoadingAction(loadingState: LoadingState.idle);
       return state.copyWith(
         homePageState: state.homePageState.copyWith(
