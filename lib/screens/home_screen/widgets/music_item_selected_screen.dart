a15f88e1b380ed94c09d75d28a45a6aaf7154c92
darCavalier11 <sumit.pradhan65@gmail.com>
2022-10-04 23:29:31 +0530

    finished the animation

--- lib/screens/home_screen/widgets/music_item_selected_screen.dart
+++ lib/screens/home_screen/widgets/music_item_selected_screen.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
@@ -11,7 +12,7 @@ import 'package:music_player/widgets/app_primary_button.dart';
 
 import '../../../redux/models/music_item.dart';
 
-class MusicItemSelectedScreen extends StatelessWidget {
+class MusicItemSelectedScreen extends StatefulWidget {
   static const routeName = '/musicItemSelectedScreen';
   final MusicItem musicItem;
   const MusicItemSelectedScreen({
@@ -20,108 +21,111 @@ class MusicItemSelectedScreen extends StatelessWidget {
   }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: Stack(
-        children: [
-          BackdropFilter(
-            filter: ImageFilter.blur(
-              sigmaX: 25,
-              sigmaY: 25,
-            ),
-            child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
-                color: Theme.of(context).canvasColor.withOpacity(0.1),
-              ),
-            ),
-          ),
-          Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Hero(
-                  tag: musicItem.musicId,
-                  child: MusicListTile(
-                    selectedMusic: musicItem,
-                    onTap: (p) {},
-                  ),
-                ),
-                _ButtonRowWidget(),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
+  State<MusicItemSelectedScreen> createState() =>
+      _MusicItemSelectedScreenState();
 }
 
-class _ButtonRowWidget extends StatefulWidget {
-  const _ButtonRowWidget({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<_ButtonRowWidget> createState() => _ButtonRowWidgetState();
-}
-
-class _ButtonRowWidgetState extends State<_ButtonRowWidget>
+class _MusicItemSelectedScreenState extends State<MusicItemSelectedScreen>
     with SingleTickerProviderStateMixin {
   late final AnimationController _animationController;
 
   @override
   void initState() {
-    super.initState();
     _animationController = AnimationController(
       vsync: this,
       duration: const Duration(milliseconds: 400),
     );
     _animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedBuilder(
-      animation: _animationController,
-      builder: (context, child) {
-        return Opacity(
-          opacity: _animationController.value,
-          child: Transform.translate(
-            offset: Offset(0, (1 - _animationController.value) * 50),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                IconButton(
-                  padding: const EdgeInsets.all(0),
-                  splashColor: Colors.redAccent.withOpacity(0.2),
-                  onPressed: () {},
-                  icon: const Icon(
-                    CupertinoIcons.heart,
-                    color: Colors.redAccent,
-                  ),
-                ),
-                const AppPrimaryButton(
-                  buttonText: 'Add To Playlist',
-                  trailingIcon: Icon(
-                    Iconsax.music_playlist,
-                    size: 18,
-                  ),
-                ),
-                const AppPrimaryButton(
-                  buttonText: 'Show next',
-                  trailingIcon: Icon(
-                    CupertinoIcons.list_bullet,
-                    size: 18,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        );
+    return WillPopScope(
+      onWillPop: () async {
+        _animationController.reverse();
+        return true;
       },
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        body: Stack(
+          children: [
+            BackdropFilter(
+              filter: ImageFilter.blur(
+                sigmaX: 25,
+                sigmaY: 25,
+              ),
+              child: Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height,
+                decoration: BoxDecoration(
+                  color: Theme.of(context).canvasColor.withOpacity(0.1),
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Hero(
+                    tag: widget.musicItem.musicId,
+                    child: MusicListTile(
+                      selectedMusic: widget.musicItem,
+                      onTap: (p) {},
+                    ),
+                  ),
+                  AnimatedBuilder(
+                    animation: _animationController,
+                    builder: (context, child) {
+                      return Opacity(
+                        opacity: _animationController.value,
+                        child: Transform.translate(
+                          offset:
+                              Offset(0, (1 - _animationController.value) * 50),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              IconButton(
+                                padding: const EdgeInsets.all(0),
+                                splashColor: Colors.redAccent.withOpacity(0.2),
+                                onPressed: () {},
+                                icon: const Icon(
+                                  CupertinoIcons.heart,
+                                  color: Colors.redAccent,
+                                ),
+                              ),
+                              const AppPrimaryButton(
+                                buttonText: 'Add To Playlist',
+                                trailingIcon: Icon(
+                                  Iconsax.music_playlist,
+                                  size: 18,
+                                ),
+                              ),
+                              const AppPrimaryButton(
+                                buttonText: 'Show next',
+                                trailingIcon: Icon(
+                                  CupertinoIcons.list_bullet,
+                                  size: 18,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    },
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
