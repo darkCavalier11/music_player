b2c6b8a5c0fd77179a5bfa578f3ba0e4aae0a292
darCavalier11 <sumit.pradhan65@gmail.com>
2022-10-07 22:14:04 +0530

    added more ui bits

--- lib/screens/home_screen/widgets/select_playlist_add_music_screen.dart
+++ lib/screens/home_screen/widgets/select_playlist_add_music_screen.dart
@@ -1,19 +1,27 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:developer';
 import 'dart:ui';
 
 import 'package:async_redux/async_redux.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 import 'package:music_player/redux/models/app_state.dart';
 import 'package:music_player/redux/models/user_playlist_list_item.dart';
 import 'package:music_player/widgets/app_primary_button.dart';
 import 'package:music_player/widgets/app_text_field.dart';
-import 'package:music_player/widgets/search_text_field.dart';
 
-class SelectMusicAddMusicScreen extends StatelessWidget {
+class SelectMusicAddMusicScreen extends StatefulWidget {
   const SelectMusicAddMusicScreen({Key? key}) : super(key: key);
 
+  @override
+  State<SelectMusicAddMusicScreen> createState() =>
+      _SelectMusicAddMusicScreenState();
+}
+
+class _SelectMusicAddMusicScreenState extends State<SelectMusicAddMusicScreen> {
+  List<int> _selectedTileIndexes = [];
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, _ViewModel>(
@@ -50,9 +58,22 @@ class SelectMusicAddMusicScreen extends StatelessWidget {
                   Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      Container(
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                          'Select Playlist to Add',
+                          style:
+                              Theme.of(context).textTheme.bodyLarge?.copyWith(
+                                    fontSize: 20,
+                                    color: Theme.of(context).disabledColor,
+                                  ),
+                        ),
+                      ),
+                      AnimatedContainer(
+                        duration: const Duration(milliseconds: 200),
                         width: MediaQuery.of(context).size.width * 0.8,
-                        height: MediaQuery.of(context).size.width * 0.8,
+                        height: MediaQuery.of(context).size.width *
+                            (_selectedTileIndexes.isEmpty ? 0.8 : 0.9),
                         decoration: BoxDecoration(
                           color: Theme.of(context).canvasColor,
                           borderRadius: BorderRadius.circular(20),
@@ -60,30 +81,47 @@ class SelectMusicAddMusicScreen extends StatelessWidget {
                         child: Column(
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Padding(
-                              padding: const EdgeInsets.symmetric(
-                                  horizontal: 24, vertical: 16),
-                              child: Text(
-                                'Select Playlist to Add',
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge
-                                    ?.copyWith(
-                                      color: Theme.of(context).disabledColor,
-                                    ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.only(
+                                    bottom: 8, left: 8, right: 8),
+                                child: ListView.builder(
+                                  padding: const EdgeInsets.all(0),
+                                  itemBuilder: (context, index) => ListTile(
+                                    title: Text('Hello'),
+                                    onTap: () {
+                                      if (_selectedTileIndexes
+                                          .contains(index)) {
+                                        _selectedTileIndexes.remove(index);
+                                      } else {
+                                        _selectedTileIndexes.add(index);
+                                      }
+                                      setState(() {});
+                                    },
+                                    trailing: !_selectedTileIndexes
+                                            .contains(index)
+                                        ? const Icon(CupertinoIcons.circle)
+                                        : Icon(
+                                            Iconsax.tick_circle,
+                                            color:
+                                                Theme.of(context).primaryColor,
+                                          ),
+                                  ),
+                                  itemCount: 10,
+                                ),
                               ),
                             ),
-                            Expanded(
-                              child: ListView.builder(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 10),
-                                itemBuilder: (context, index) => ListTile(
-                                  title: Text('Hello'),
-                                  onTap: () {},
-                                ),
-                                itemCount: 10,
+                            if (_selectedTileIndexes.isNotEmpty)
+                              Row(
+                                children: [
+                                  const Spacer(),
+                                  AppPrimaryButton(
+                                    buttonText: 'Done',
+                                    onTap: () {},
+                                    trailingIcon: Icons.done,
+                                  ),
+                                ],
                               ),
-                            )
                           ],
                         ),
                       ),
@@ -147,7 +185,7 @@ class _ViewModel extends Vm {
   });
 }
 
-class _Factory extends VmFactory<AppState, SelectMusicAddMusicScreen> {
+class _Factory extends VmFactory<AppState, _SelectMusicAddMusicScreenState> {
   _Factory(widget) : super(widget);
   @override
   _ViewModel fromStore() {
