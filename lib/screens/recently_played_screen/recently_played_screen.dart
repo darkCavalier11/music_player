44b97ca6fbc83068f038212216a37b64af4f2971
darCavalier11 <sumit.pradhan65@gmail.com>
2022-10-16 22:00:56 +0530

    completed the recently played screen

--- lib/screens/recently_played_screen/recently_played_screen.dart
+++ lib/screens/recently_played_screen/recently_played_screen.dart
@@ -1,7 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:music_player/redux/models/app_state.dart';
+import 'package:music_player/redux/models/music_item.dart';
+import 'package:music_player/screens/home_screen/widgets/music_list_tile.dart';
 import 'package:music_player/widgets/app_text_field.dart';
 import 'package:music_player/widgets/search_text_field.dart';
 
@@ -12,44 +18,77 @@ class RecentlyPlayedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24, top: 64),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const AppTextField(
-              hintText: 'Search Recently played Items...',
-              leadingIcon: Icons.search,
-            ),
-            const SizedBox(height: 10),
-            Hero(
-              tag: 'RECENTLY_PLAYED',
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 24),
-                child: Row(
-                  children: [
-                    Icon(
-                      CupertinoIcons.stopwatch,
-                      size: 35,
-                      color: Theme.of(context).primaryColor,
-                    ),
-                    const SizedBox(width: 10),
-                    Text(
-                      'Recently played',
-                      style: Theme.of(context).textTheme.button?.copyWith(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColor,
-                          ),
-                    ),
-                  ],
+    return StoreConnector<AppState, _ViewModel>(
+      vm: () => _Factory(this),
+      builder: (context, snapshot) {
+        return Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(left: 24, right: 24, top: 64),
+                child: AppTextField(
+                  hintText: 'Search Recently played Items...',
+                  leadingIcon: Icons.search,
                 ),
               ),
-            ),
-          ],
-        ),
-      ),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: Hero(
+                  tag: 'RECENTLY_PLAYED',
+                  child: Row(
+                    children: [
+                      Icon(
+                        CupertinoIcons.stopwatch,
+                        size: 35,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      const SizedBox(width: 10),
+                      Text(
+                        'Recently played',
+                        style: Theme.of(context).textTheme.button?.copyWith(
+                              fontSize: 30,
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  padding: const EdgeInsets.all(0),
+                  itemBuilder: (context, index) {
+                    return MusicListTile(
+                      selectedMusic: snapshot.recentlyPlayedList[index],
+                    );
+                  },
+                  itemCount: snapshot.recentlyPlayedList.length,
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ViewModel extends Vm {
+  final List<MusicItem> recentlyPlayedList;
+  _ViewModel({
+    required this.recentlyPlayedList,
+  });
+}
+
+class _Factory extends VmFactory<AppState, RecentlyPlayedScreen> {
+  _Factory(widget) : super(widget);
+  @override
+  _ViewModel fromStore() {
+    return _ViewModel(
+      recentlyPlayedList: state.homePageState.recentlyPlayedMusicList,
     );
   }
 }
