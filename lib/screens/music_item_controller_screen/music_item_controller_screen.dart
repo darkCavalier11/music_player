c8dbe6b6649b7aae6429e2b833ad8a4c946e822c
darCavalier11 <sumit.pradhan65@gmail.com>
2022-11-02 21:29:34 +0530

    finished the initial setup for playlist view

--- lib/screens/music_item_controller_screen/music_item_controller_screen.dart
+++ lib/screens/music_item_controller_screen/music_item_controller_screen.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 
 import 'package:music_player/redux/models/app_state.dart';
 import 'package:music_player/redux/models/music_item.dart';
+import 'package:music_player/screens/home_screen/actions/music_actions.dart';
 import 'package:music_player/screens/home_screen/widgets/music_player_widget.dart';
 
 class MusicListItemControllerScreen extends StatelessWidget {
@@ -35,103 +36,110 @@ class MusicListItemControllerScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              const Align(
-                child: MusicPlayerWidget(),
-                alignment: Alignment(0, 0),
+              const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Align(
+                  child: MusicPlayerWidget(),
+                  alignment: Alignment(0, 0),
+                ),
               ),
               Align(
-                alignment: const Alignment(0, 1.5),
+                alignment: const Alignment(0, 3.5),
                 child: CarouselSlider(
                   items: List.generate(snapshot.nexMusicList.length, (index) {
                     return Column(
                       children: [
-                        Container(
-                          padding: const EdgeInsets.all(12),
-                          decoration: BoxDecoration(
-                            color: Theme.of(context)
-                                .disabledColor
-                                .withOpacity(0.1),
-                            borderRadius: BorderRadius.circular(24),
-                          ),
-                          child: Column(
-                            children: [
-                              ClipRRect(
-                                borderRadius: BorderRadius.circular(100),
-                                child: Stack(
-                                  alignment: Alignment.center,
+                        GestureDetector(
+                          onTap: () {
+                            snapshot.playAudio(snapshot.nexMusicList[index]);
+                          },
+                          child: Container(
+                            padding: const EdgeInsets.all(12),
+                            width: 200,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(24),
+                            ),
+                            child: Column(
+                              children: [
+                                ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: Stack(
+                                    alignment: Alignment.center,
+                                    children: [
+                                      Image.network(
+                                        snapshot.nexMusicList[index].imageUrl,
+                                        width: 150,
+                                        height: 150,
+                                        fit: BoxFit.cover,
+                                      ),
+                                      Container(
+                                        height: 40,
+                                        width: 40,
+                                        decoration: BoxDecoration(
+                                          color: Colors.black,
+                                          shape: BoxShape.circle,
+                                          border: Border.all(
+                                            color:
+                                                Theme.of(context).disabledColor,
+                                            width: 3,
+                                          ),
+                                        ),
+                                      ),
+                                      Container(
+                                        height: 140,
+                                        width: 140,
+                                        decoration: BoxDecoration(
+                                          color: Colors.transparent,
+                                          shape: BoxShape.circle,
+                                          border: Border.all(
+                                            color:
+                                                Theme.of(context).disabledColor,
+                                            width: 2,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Text(
+                                  snapshot.nexMusicList[index].title,
+                                  maxLines: 2,
+                                  overflow: TextOverflow.fade,
+                                  softWrap: false,
+                                ),
+                                const Divider(),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
                                   children: [
-                                    Image.network(
-                                      snapshot.nexMusicList[index].imageUrl,
-                                      width: 150,
-                                      height: 150,
-                                      fit: BoxFit.cover,
-                                    ),
-                                    Container(
-                                      height: 40,
-                                      width: 40,
-                                      decoration: BoxDecoration(
-                                        color: Colors.black,
-                                        shape: BoxShape.circle,
-                                        border: Border.all(
-                                          color:
-                                              Theme.of(context).disabledColor,
-                                          width: 3,
-                                        ),
+                                    Expanded(
+                                      child: Text(
+                                        snapshot.nexMusicList[index].author,
+                                        maxLines: 1,
+                                        overflow: TextOverflow.ellipsis,
+                                        style:
+                                            Theme.of(context).textTheme.caption,
                                       ),
+                                      flex: 4,
                                     ),
-                                    Container(
-                                      height: 140,
-                                      width: 140,
-                                      decoration: BoxDecoration(
-                                        color: Colors.transparent,
-                                        shape: BoxShape.circle,
-                                        border: Border.all(
-                                          color:
-                                              Theme.of(context).disabledColor,
-                                          width: 2,
-                                        ),
-                                      ),
+                                    Expanded(
+                                      child: Text(snapshot
+                                          .nexMusicList[index].duration),
+                                      flex: 1,
                                     ),
                                   ],
                                 ),
-                              ),
-                              Text(
-                                snapshot.nexMusicList[index].title,
-                                maxLines: 2,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                              const Divider(),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceAround,
-                                children: [
-                                  Expanded(
-                                    child: Text(
-                                      snapshot.nexMusicList[index].author,
-                                      maxLines: 1,
-                                      overflow: TextOverflow.ellipsis,
-                                      style:
-                                          Theme.of(context).textTheme.caption,
-                                    ),
-                                    flex: 4,
-                                  ),
-                                  Expanded(
-                                    child: Text(
-                                        snapshot.nexMusicList[index].duration),
-                                    flex: 1,
-                                  ),
-                                ],
-                              ),
-                            ],
+                              ],
+                            ),
                           ),
                         ),
                       ],
                     );
                   }),
                   options: CarouselOptions(
-                    aspectRatio: 1.0,
+                    aspectRatio: 0.64,
                     pageSnapping: false,
-                    viewportFraction: 0.5,
+                    viewportFraction: 0.6,
                     scrollPhysics: const BouncingScrollPhysics(),
                     scrollDirection: Axis.horizontal,
                     enlargeCenterPage: true,
@@ -151,9 +159,11 @@ class MusicListItemControllerScreen extends StatelessWidget {
 class _ViewModel extends Vm {
   final List<MusicItem> nexMusicList;
   final MusicItem? selectedMusicItem;
+  final Future<void> Function(MusicItem) playAudio;
   _ViewModel({
     required this.nexMusicList,
     required this.selectedMusicItem,
+    required this.playAudio,
   }) : super(equals: [nexMusicList]);
 }
 
@@ -162,6 +172,9 @@ class _Factory extends VmFactory<AppState, MusicListItemControllerScreen> {
   @override
   _ViewModel fromStore() {
     return _ViewModel(
+      playAudio: (musicItem) async {
+        dispatch(PlayAudioAction(musicItem: musicItem));
+      },
       nexMusicList: state.audioPlayerState.nextMusicList,
       selectedMusicItem: state.audioPlayerState.selectedMusic,
     );
