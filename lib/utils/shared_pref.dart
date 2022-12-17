a7d421378d3bbdf6b452df84541a43a913eb509a
darCavalier11 <sumit.pradhan65@gmail.com>
2022-12-17 23:47:19 +0530

    added shared pref operations

--- lib/utils/shared_pref.dart
+++ lib/utils/shared_pref.dart
@@ -1,1 +1,7 @@
-class SharedPref {}
+class SharedPrefKeys {
+  static const String userProfileUsername = 'userProfileUsername';
+  static const String userProfileprofilePicPlatformPath =
+      'userProfileProfilePicPlatformPath';
+  static const String userProfileisOnBoardingDone =
+      'userProfileisOnBoardingDone';
+}
