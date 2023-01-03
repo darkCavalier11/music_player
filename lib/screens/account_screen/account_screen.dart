6596c9eb6467695c9d973baffcfc52a55f7f7664
darCavalier11 <sumit.pradhan65@gmail.com>
2023-01-03 21:37:57 +0530

    added vm to account screen

--- lib/screens/account_screen/account_screen.dart
+++ lib/screens/account_screen/account_screen.dart
@@ -1,10 +1,13 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:async_redux/async_redux.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+
 import 'package:music_player/main.dart';
 import 'package:music_player/screens/onboarding/onboarding.dart';
 
+import '../../redux/models/app_state.dart';
 import '../../widgets/text_themes/app_header_text.dart';
 
 class AccountScreen extends StatelessWidget {
@@ -12,78 +15,101 @@ class AccountScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.only(left: 24, right: 24, top: 64),
-        child: Column(
-          children: [
-            const AppHeaderText(
-              icon: Iconsax.timer_1,
-              text: 'My Account',
-            ),
-            const SizedBox(height: 10),
-            Stack(
+    return StoreConnector<AppState, _ViewModel>(
+      vm: () => _Factory(this),
+      builder: (context, snapshot) {
+        return Scaffold(
+          body: Padding(
+            padding: const EdgeInsets.only(left: 24, right: 24, top: 64),
+            child: Column(
               children: [
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: CircleAvatar(
-                    radius: 35,
-                    child: Text(
-                      'S',
-                      style: Theme.of(context).textTheme.headline4?.copyWith(
-                            color: Theme.of(context).cardColor,
-                          ),
-                    ),
-                  ),
+                const AppHeaderText(
+                  icon: Iconsax.timer_1,
+                  text: 'My Account',
                 ),
-                Positioned(
-                  right: 0,
-                  bottom: 6,
-                  child: IconButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const OnboardingScreen()));
-                    },
-                    icon: Icon(
-                      CupertinoIcons.pencil_circle_fill,
-                      color: Theme.of(context).backgroundColor,
+                const SizedBox(height: 10),
+                Stack(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.all(16),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      child: CircleAvatar(
+                        radius: 35,
+                        child: Text(
+                          'S',
+                          style:
+                              Theme.of(context).textTheme.headline4?.copyWith(
+                                    color: Theme.of(context).cardColor,
+                                  ),
+                        ),
+                      ),
                     ),
-                  ),
+                    Positioned(
+                      right: 0,
+                      bottom: 6,
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(
+                              builder: (context) => const OnboardingScreen()));
+                        },
+                        icon: Icon(
+                          CupertinoIcons.pencil_circle_fill,
+                          color: Theme.of(context).backgroundColor,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
+                Text(
+                  snapshot.userName,
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        color: Theme.of(context).disabledColor,
+                        fontWeight: FontWeight.bold,
+                      ),
+                ),
+                AccountTileSwitch(
+                  description:
+                      'Based on your music playing item, it will cache most played items',
+                  title: 'Intelligent Cache',
+                  onChanged: (v) {},
+                ),
+                const Spacer(),
+                Text('v' + appVersion),
+                Text(
+                  'Made with ❤️ in 🇮🇳',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                Text(
+                  'About',
+                  style: Theme.of(context).textTheme.caption?.copyWith(
+                        decoration: TextDecoration.underline,
+                      ),
+                ),
+                const SizedBox(height: 100),
               ],
             ),
-            Text(
-              'Sumit Kumar Pradhan',
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                    color: Theme.of(context).disabledColor,
-                    fontWeight: FontWeight.bold,
-                  ),
-            ),
-            AccountTileSwitch(
-              description:
-                  'Based on your music playing item, it will cache most played items',
-              title: 'Intelligent Cache',
-              onChanged: (v) {},
-            ),
-            const Spacer(),
-            Text('v' + appVersion),
-            Text(
-              'Made with ❤️ in 🇮🇳',
-              style: Theme.of(context).textTheme.caption,
-            ),
-            Text(
-              'About',
-              style: Theme.of(context).textTheme.caption?.copyWith(
-                    decoration: TextDecoration.underline,
-                  ),
-            ),
-            const SizedBox(height: 100),
-          ],
-        ),
-      ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _ViewModel extends Vm {
+  final String userName;
+  _ViewModel({
+    required this.userName,
+  });
+}
+
+class _Factory extends VmFactory<AppState, AccountScreen> {
+  _Factory(widget) : super(widget);
+  @override
+  _ViewModel fromStore() {
+    return _ViewModel(
+      userName: state.userProfileState.userName,
     );
   }
 }
--- lib/screens/onboarding/onboarding.dart
+++ lib/screens/onboarding/onboarding.dart
@@ -38,128 +38,134 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
   @override
   Widget build(BuildContext context) {
     return StoreConnector<AppState, _ViewModel>(
+      onInit: (store) {
+        _textEditingController.text = store.state.userProfileState.userName;
+      },
       vm: () => _Factory(this),
-      builder: (context, snapshot) => Scaffold(
-        body: Stack(
-          fit: StackFit.expand,
-          children: [
-            Positioned(
-              child: Image.asset(
-                'assets/images/onboarding_background.jpeg',
-                height: MediaQuery.of(context).size.height * 0.26,
-                width: MediaQuery.of(context).size.width,
-                fit: BoxFit.cover,
+      builder: (context, snapshot) {
+        return Scaffold(
+          body: Stack(
+            fit: StackFit.expand,
+            children: [
+              Positioned(
+                child: Image.asset(
+                  'assets/images/onboarding_background.jpeg',
+                  height: MediaQuery.of(context).size.height * 0.26,
+                  width: MediaQuery.of(context).size.width,
+                  fit: BoxFit.cover,
+                ),
+                top: 0,
               ),
-              top: 0,
-            ),
-            Positioned(
-              bottom: 0,
-              left: 0,
-              child: Container(
-                padding: const EdgeInsets.all(24),
-                height: MediaQuery.of(context).size.height * 0.8,
-                width: MediaQuery.of(context).size.width,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).scaffoldBackgroundColor,
-                  borderRadius: const BorderRadius.only(
-                    topLeft: Radius.circular(36),
-                    topRight: Radius.circular(36),
+              Positioned(
+                bottom: 0,
+                left: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(24),
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).scaffoldBackgroundColor,
+                    borderRadius: const BorderRadius.only(
+                      topLeft: Radius.circular(36),
+                      topRight: Radius.circular(36),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      Stack(
+                        clipBehavior: Clip.hardEdge,
+                        children: [
+                          Positioned(
+                            left: 20,
+                            top: 20,
+                            child: MusicPlayingWaveWidget(
+                              playingStream: Stream.value(true),
+                            ),
+                          ),
+                          Text(
+                            'Listen everything adfree!',
+                            style:
+                                Theme.of(context).textTheme.headline2?.copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: Theme.of(context).cardColor,
+                                    ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      Stack(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Theme.of(context).dividerColor,
+                            radius: 80,
+                          ),
+                          Positioned(
+                            child: GestureDetector(
+                              child: const Icon(
+                                CupertinoIcons.camera_circle,
+                                size: 35,
+                              ),
+                              onTap: () async {
+                                try {
+                                  FilePickerResult? result =
+                                      await FilePicker.platform.pickFiles(
+                                    allowMultiple: false,
+                                    type: FileType.image,
+                                  );
+
+                                  if (result?.files.single.path != null) {
+                                    File file =
+                                        File(result!.files.single.path!);
+                                    log('${file.absolute}');
+                                  } else {}
+                                } catch (err) {
+                                  log('$err');
+                                }
+                              },
+                            ),
+                            right: 12,
+                            bottom: 12,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(height: 40),
+                      AppTextField(
+                        enabled: true,
+                        hintText: 'Enter Your Name',
+                        textEditingController: _textEditingController,
+                        // todo: handle this more gracefully
+                        onChanged: (c) {
+                          setState(() {});
+                        },
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          const Spacer(),
+                          AppPrimaryButton(
+                            onTap: () {
+                              snapshot.setProfilePicPlatformPath(
+                                  profilePicPlatformPath: '');
+                              snapshot.setUsername(
+                                  userName: _textEditingController.text);
+                              snapshot.setOnboardingDone();
+                              Navigator.of(context).pop();
+                            },
+                            buttonText: 'Let\'s Go',
+                            trailingIcon: Icons.arrow_circle_right,
+                            disabled: (!snapshot.isOnBoardingDone &&
+                                _textEditingController.text == ''),
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
-                child: Column(
-                  children: [
-                    Stack(
-                      clipBehavior: Clip.hardEdge,
-                      children: [
-                        Positioned(
-                          left: 20,
-                          top: 20,
-                          child: MusicPlayingWaveWidget(
-                            playingStream: Stream.value(true),
-                          ),
-                        ),
-                        Text(
-                          'Listen everything adfree!',
-                          style:
-                              Theme.of(context).textTheme.headline2?.copyWith(
-                                    fontWeight: FontWeight.bold,
-                                    color: Theme.of(context).cardColor,
-                                  ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 40),
-                    Stack(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor: Theme.of(context).dividerColor,
-                          radius: 80,
-                        ),
-                        Positioned(
-                          child: GestureDetector(
-                            child: const Icon(
-                              CupertinoIcons.camera_circle,
-                              size: 35,
-                            ),
-                            onTap: () async {
-                              try {
-                                FilePickerResult? result =
-                                    await FilePicker.platform.pickFiles(
-                                  allowMultiple: false,
-                                  type: FileType.image,
-                                );
-
-                                if (result?.files.single.path != null) {
-                                  File file = File(result!.files.single.path!);
-                                  log('${file.absolute}');
-                                } else {}
-                              } catch (err) {
-                                log('$err');
-                              }
-                            },
-                          ),
-                          right: 12,
-                          bottom: 12,
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: 40),
-                    AppTextField(
-                      enabled: true,
-                      hintText: 'Enter Your Name',
-                      textEditingController: _textEditingController,
-                      // todo: handle this more gracefully
-                      onChanged: (c) {
-                        setState(() {});
-                      },
-                    ),
-                    const Spacer(),
-                    Row(
-                      children: [
-                        const Spacer(),
-                        AppPrimaryButton(
-                          onTap: () {
-                            snapshot.setProfilePicPlatformPath(
-                                profilePicPlatformPath: '');
-                            snapshot.setUsername(
-                                userName: _textEditingController.text);
-                            snapshot.setOnboardingDone();
-                            Navigator.of(context).pop();
-                          },
-                          buttonText: 'Let\'s Go',
-                          trailingIcon: Icons.arrow_circle_right,
-                          disabled: (!snapshot.isOnBoardingDone &&
-                              _textEditingController.text == ''),
-                        )
-                      ],
-                    ),
-                  ],
-                ),
               ),
-            ),
-          ],
-        ),
-      ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
