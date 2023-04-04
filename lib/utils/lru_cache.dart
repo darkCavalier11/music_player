e0248c7dcda5f538bf5d1c9e88739add323f6116
darCavalier11 <sumit.pradhan65@gmail.com>
2023-04-04 21:42:09 +0530

    initialised test

--- lib/utils/lru_cache.dart
+++
@@ -1,7 +0,0 @@
-// a generic cache storage wrapped around shared preferences.
-class LruCache {
-  // This specifies how much music key-value pair should be stored
-  // A key is the musicId and the value is the musicUrl.
-  // todo : resolve IP address in the url to make the url work as cache.
-  static const MUSIC_KEY_LIMIT = 1000;
-}
