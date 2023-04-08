f0ae6cccb78736498b0267ce81a749bae9208a46
darCavalier11 <sumit.pradhan65@gmail.com>
2023-04-08 21:00:07 +0530

    added bottom nav index change test

--- test/redux_test/ui_action_test.dart
+++ test/redux_test/ui_action_test.dart
@@ -1,5 +1,19 @@
+import 'package:async_redux/async_redux.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:music_player/redux/action/ui_action.dart';
+import 'package:music_player/redux/models/app_state.dart';
 
 void main() {
-  group('UI redux actions test', () { })  ;
+  WidgetsFlutterBinding.ensureInitialized();
+  group('UI redux actions test', () {
+    var store = Store<AppState>(initialState: AppState.initial());
+    var storeTester = StoreTester.from(store);
+
+    test('Test bottom navgation change', () async {
+      storeTester.dispatch(ChangeBottomNavIndex(index: 1));
+      final info = await storeTester.wait(ChangeBottomNavIndex);
+      expect(info.state.uiState.currentBottomNavIndex, 1);
+    });
+  });
 }
